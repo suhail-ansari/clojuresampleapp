22897a07f03411daee9e0cb057e875cc7460f2ab
suhail-ansari <suhail.ism@gmail.com>
2014-01-08 15:53:58 +0530

    improved more code

--- src/test2/core.clj
+++ src/test2/core.clj
@@ -1,15 +1,16 @@
 (ns test2.core
+	(:gen-class)
 	(:use [compojure.route :only [files not-found]]
-      [compojure.handler :only [site]]
-      [compojure.core :only [defroutes GET POST DELETE ANY context]]
-      org.httpkit.server)
-	(:use [cheshire.core :only [generate-string parse-string]])
-	(:require [clojure.java.jdbc :as j])
-	(:gen-class))
+	      [compojure.handler :only [site]]
+	      [compojure.core :only [defroutes GET POST DELETE ANY context]]
+	      [cheshire.core :only [generate-string parse-string]]
+	      [ring.middleware.session :only [wrap-session]]
+	      org.httpkit.server)
+	(:require [clojure.java.jdbc :as j]))
 
 
 (def not-contains? (complement contains?))
-
+(def not-nil? (complement nil?))
 
 ;;define and connect mysql;;
 (def mysql-db {:subprotocol "mysql"
@@ -19,7 +20,6 @@
 
 ;;write data :data  to database :db
 (defn write-data [data]
-	(println data)
 	(if (or (empty? data) (not-contains? data :db) (not-contains? data :data))
 		[102 "empty/invalid parameters"]
 		(do (try (j/insert! mysql-db (keyword (:db data)) (:data data)) [200 "OK"]
@@ -27,38 +27,41 @@
 
 
 ;;get + jsonp request
-(defn get-handler [{params :params}]
-	(let [result (write-data (apply dissoc params [:_ :callback]))]
-		(do (if (contains? params :callback)
-				(do {:status 200
-					:headers {"Content-Type" "application/json" "Connection" "close"}
-					:body (str (:callback params) "(" (generate-string {:status (nth result 0) :message (nth result 1)}) ");")
-				})
-				(do {:status 200
-					:headers {"Content-Type" "application/json"}
-					:body (str (generate-string {:status (nth result 0) :message (nth result 1)}))
-				})))))
+(defn get-handler [request]
+	(let [result (write-data (-> request :params (dissoc [:_ :callback]))) callback (-> request :params :callback )]
+		{:status 200
+			:headers {"Content-Type" "application/json" "Connection" "close"}
+			:body (if (not-nil? callback) 
+					(do (str callback "(" (generate-string {:status (first result) :message (second result)}) ");"))
+				  	(do (str (generate-string {:status (first result) :message (second result)}))))
+			}))
 
 ;;post request
-(defn post-handler [{params :params}]
-	(let [result (write-data (apply dissoc params [:_ :callback]))]
+(defn post-handler [request]
+	(println (:session request))
+	(let [result (write-data (-> request :params (dissoc [:_ :callback])))]
 		(do {:status 200
 					:headers {"Content-Type" "application/json" "Connection" "close"}
-					:body (str (generate-string {:status (nth result 0) :message (nth result 1)}))
+					:body (str (generate-string {:status (first result) :message (second result)}))
 				})))
 
+
 ;;404 error handler
-(defn not-found-handler [params]
+(defn not-found-handler [request]
 	{:status 404
 	      :headers {"Content-Type" "application/json" "Connection" "close"}
 	      :body  (str (generate-string {:status 404 :message "not found"}))})
 
 
+
 (defroutes main-routes
-	(GET "/" {params :params} get-handler)
-	(POST "/" {params :params} post-handler)
+	(GET "/" request get-handler)
+	(POST "/" request post-handler)
 	(not-found not-found-handler))
 
-(defn -main []
-	(println "main start")
-	(run-server (site #'main-routes) {:port 8080}))
+
+(def app (site #'main-routes))
+
+(defn -main [& args]
+	(run-server app {:port 8080 ::queue-size 204800})
+	(println "Started Server @ 127.0.0.1:8080"))
